97c9fba3e16bebc2ec66254b2d567f6809309f69
Mat Trudel <mat@geeky.net>
2018-02-14 16:51:46 -0500

    Bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SchedEx.Mixfile do
   def project do
     [
       app: :sched_ex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       description: description(),
