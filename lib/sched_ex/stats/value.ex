677d4114afb33fe0ee22eb07f948cc869018c473
Mat Trudel <mat@geeky.net>
2018-04-07 15:01:32 -0400

    Use a moving average

--- lib/sched_ex/stats/value.ex
+++ lib/sched_ex/stats/value.ex
@@ -1,11 +1,14 @@
 defmodule SchedEx.Stats.Value do
-  defstruct min: nil, max: 0, avg: 0, count: 0
+  defstruct min: nil, max: nil, avg: nil, count: 0
+
+  @num_periods 10
+  @weight_factor 2 / (@num_periods + 1)
 
   def update(%__MODULE__{min: min, max: max, avg: avg, count: count}, sample) do
     %__MODULE__{
       min: min(min, sample),
-      max: max(max, sample),
-      avg: ((count * avg) + sample) / (count + 1),
+      max: max && max(max, sample) || sample,
+      avg: avg && (((sample - avg) * @weight_factor) + avg) || sample,
       count: count + 1
     }
   end
