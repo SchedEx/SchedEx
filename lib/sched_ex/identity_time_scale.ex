3b935e6bfa9494a226a07ce962d68e1792bf7a6c
Jason Axelson <jason.axelson@gmail.com>
2018-11-12 06:19:35 -1000

    Add a timescale behaviour
    Makes it more obvious and explicit what is needed to implement a custom
    timescale.

--- lib/sched_ex/identity_time_scale.ex
+++ lib/sched_ex/identity_time_scale.ex
@@ -1,10 +1,15 @@
 defmodule SchedEx.IdentityTimeScale do
-  @moduledoc false
+  @moduledoc """
+  The default module used to set the `time_scale`. Can be thought of as "normal time" where "now" is now and speedup is 1 (no speedup)
+  """
+  @behaviour SchedEx.TimeScale
 
+  @impl true
   def now(timezone) do
     Timex.now(timezone)
   end
 
+  @impl true
   def speedup do
     1
   end
